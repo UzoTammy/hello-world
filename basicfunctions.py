73b799c3367fb449477e8ba5ebe1470942d09330
Uzo Tammy Nwokoro <65567233+UzoTammy@users.noreply.github.com>
2020-05-31 18:18:16 +0100

    added divide func

--- basicfunctions.py
+++ basicfunctions.py
@@ -1,6 +1,6 @@
+"""Basic functions as tools for programming"""
 
-
-# function to add numbers
+# Function to convert number to currency format.
 def currency_format(num, x):
     if type(num) == float or type(num) == int:
         print(f'{num:,.{x}f}')
@@ -8,7 +8,7 @@ def currency_format(num, x):
         if type(num) == str:
             print("expecting a float or integer type but string type supplied")
 
-
+# Function to convert numeric string to currency format
 def string_to_currency(text):
     if type(text) == str:
         if text.isdigit():
@@ -22,8 +22,14 @@ def string_to_currency(text):
                 msg = "Expecting digits only, check your entry"
                 print(msg)
 
-
+# Say hello
 def say_hello(self):
     firstname = self.name.split(' ')
     'Hello'.__add__(f' {firstname[0]},')
 
+# divide function
+def divide(x, y):
+    return x/y
+
+ans = divide(2.8,3.9)
+print(ans)
